4e3461e314d6fb31544cee5596942d72e72130bf
rasulov-emirlan <rasulov.emirlan@gmail.com>
2022-02-20 20:28:18 +0600

    second try

--- solution.go
+++ solution.go
@@ -11,14 +11,20 @@ type A int
 // CalcSquare(10.0, SidesSquare)
 // CalcSquare(10.0, SidesCircle)
 
+const (
+	Square   A = 4
+	Triangle A = 3
+	Circle   A = 0
+)
+
 func CalcSquare(sideLen float64, sidesNum A) float64 {
 	switch sidesNum {
-	case 4:
-		return sideLen * sideLen
-	case 3:
-		return (math.Sqrt(3) / 4 * (sideLen * sideLen))
-	case 0:
-		return math.Pi * (sideLen * sideLen)
+	case Square:
+		return math.Sqrt(sideLen * sideLen)
+	case Triangle:
+		return math.Sqrt(math.Sqrt(3) / 4 * (sideLen * sideLen))
+	case Circle:
+		return math.Sqrt(math.Pi * (sideLen * sideLen))
 	default:
 		return 0
 	}
